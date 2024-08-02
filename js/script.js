0db2255ccfe9b15386d6ebb675e524f649115576
olyabr777 <158588741+olyabr777@users.noreply.github.com>
2024-08-02 14:16:20 +0300

    homework 3

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 'use strict';
 
-// Prompt the user for two operands
-let value1 = parseFloat(prompt("Enter the first value:"));
-let value2 = parseFloat(prompt("Enter the second value:"));
+let value1 = parseFloat(prompt("Введіть перше значення:"));
+let value2 = parseFloat(prompt("Введіть друге значення:"));
 
 let sum = value1 + value2;
 console.log(`Результат складання: ${sum}`);
@@ -13,7 +12,6 @@ console.log(`Результат віднімання: ${difference}`);
 let multiplication = value1 * value2;
 console.log(`Результат множення: ${multiplication}`);
 
-// Perform division
 if ( (value1 == 0) || (value2 == 0) ) {
     console.log("Ділення на нуль неможливо");
 } else {
