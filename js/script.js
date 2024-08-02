ed0f9447f190dac414e827253d031ee50a899afe
olyabr777 <158588741+olyabr777@users.noreply.github.com>
2024-08-02 14:00:43 +0300

    Home work 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+// 1
+let admin;
+let name;
+name = "John";
+admin = name;
+alert(admin); 
+
+// 2
+let planetName = "Earth";
+let visitorName = "Alice";
+
+// 3
+let result = "10" + 10;
+console.log(result);
+console.log(typeof result);
