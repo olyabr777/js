275c91d7f14824dcd9fd23dfa60c549832cc23d4
olyabr777 <158588741+olyabr777@users.noreply.github.com>
2024-08-04 20:48:15 +0300

    hm3

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 'use strict';
 
-let value1 = parseFloat(prompt("Введіть перше значення:"));
-let value2 = parseFloat(prompt("Введіть друге значення:"));
+const numberFirst = parseFloat(prompt("Введіть перше значення:"));
+const numberSecond = parseFloat(prompt("Введіть друге значення:"));
 
-let sum = value1 + value2;
+const sum = numberFirst + numberSecond;
 console.log(`Результат складання: ${sum}`);
 
-let difference = value1 - value2;
+const difference = numberFirst - numberSecond;
 console.log(`Результат віднімання: ${difference}`);
 
-let multiplication = value1 * value2;
+const multiplication = numberFirst * numberSecond;
 console.log(`Результат множення: ${multiplication}`);
 
-if ( (value1 == 0) || (value2 == 0) ) {
+if ( (numberFirst == 0) || (numberSecond == 0) ) {
     console.log("Ділення на нуль неможливо");
 } else {
-    let division = value1 / value2;
+    const division = numberFirst / numberSecond;
     console.log(`Результат ділення: ${division}`);
 }
