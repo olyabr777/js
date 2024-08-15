15fd86764d53638a55f246d61258447591838dfa
olyabr777 <158588741+olyabr777@users.noreply.github.com>
2024-08-15 10:20:43 +0300

    a

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 'use strict';
-const hours = parseFloat(prompt("Ведіть кількість годин"));
-const result = hours * 60;
-alert(result);
+
+// const hours = parseFloat(prompt("Ведіть кількість годин"));
+// const result = hours * 60;
+// alert(result);
 
 const userYear = parseInt(prompt("Ведіть ваш рік народження"));
 const userCity = prompt("Ведіть із якого ви міста");
 const userSport = prompt("Яким спортом ви займаєтесь?");
+
+
+const showData = confirm("Вивести данні?");
+const currentYear = new Date().getFullYear();
+
+if(showData == true) {
+    if (userCity == "Лондон" || userCity == "Київ" || userCity == "Вашингтон" ){
+        alert 
+    }
+}
 
 
 
