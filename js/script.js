2dc4674d40028b1d7c4d75e5d5fc6350b84bbc5e
olyabr777 <158588741+olyabr777@users.noreply.github.com>
2024-08-15 09:14:26 +0300

    hm_8

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
 'use strict';
-// Створити скрипт який має визначити середнє арифметичне трьох чисел з насутпними умовами:
 
-//отримати від користувача через три prompt три числа
-//показати через alert середнє арифметичне цих чисел  //
-
-const numberFirst = prompt("Ведіть перше число");
-const numberSecond = prompt("Ведіть друге число");
-const numberThird = prompt("Ведіть третє число");
+const numberFirst = parseFloat(prompt("Ведіть перше число"));
+const numberSecond = parseFloat(prompt("Ведіть друге число"));
+const numberThird = parseFloat(prompt("Ведіть третє число"));
 
 const result = (numberFirst + numberSecond + numberThird) / 3;
 alert(result);
