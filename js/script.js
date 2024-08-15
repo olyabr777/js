1313ac89dc52fe6c4085c994c400670f6b433a7f
olyabr777 <158588741+olyabr777@users.noreply.github.com>
2024-08-15 19:16:10 +0300

    hm11

--- js/script.js
+++ js/script.js
@@ -1,49 +1,18 @@
 'use strict';
 
-const userYear = parseInt(prompt("Ведіть ваш рік народження"));
-const userCity = prompt("Ведіть із якого ви міста");
-const userSport = prompt("Яким спортом ви займаєтесь?");
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
 
-const showData = confirm("Вивести данні?");
-const currentYear = new Date().getFullYear();
-
-if(showData == true) {
-    const userAge = currentYear - userYear;
-    let result = "";
-    result += `Ваш вік ${userAge} `;
-    if (userCity == "Лондон" || userCity == "Київ" || userCity == "Вашингтон" ){
-        result += `Ти живеш у столиці ${userCity} `;
-    }else{
-        result += `ти живеш у місті ${userCity} `;
-    }
-
-    let idol;
-    if(userSport == "Футбол") idol = "як Шевченко";
-    else if(userSport == "Бокс") idol = "як Усик";
-    else if(userSport == "Фехтування") idol = "як Харлан";
-    
-    if (idol != null) result += `Круто! Хочеш стати ${idol}? `;
-    else result += `${userSport} - гарний вибір!? `;
-
-    alert(result);
-
-} else {
-    let result = "";
-    if (userYear == null || userYear === "") {
-        result += "Шкода, що Ви не захотіли ввести свій рік народження ";
-    }
-    if (userCity == null || userCity === "") {
-        result += "Шкода, що Ви не захотіли ввести ваше місто ";
-    }
-    if (userSport == null || userSport === "") {
-        result += "Шкода, що Ви не захотіли ввести свій улюблений спорт ";
-    }
-    alert(result);
+switch (true) {
+    case numOrStr === null:
+        console.log('ви скасували');
+        break;
+    case numOrStr.trim() === '' :
+        console.log('Empty String');
+        break;
+    case  isNaN( +numOrStr )  :
+        console.log('number is Ba_NaN');
+        break;
+    default:
+     console.log('OK!')   
 }
-
-
-
-
-
-
-
