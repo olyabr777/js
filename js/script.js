6951003bac7fb3d98bd7467c5a486712c762345e
olyabr777 <158588741+olyabr777@users.noreply.github.com>
2024-08-15 09:47:58 +0300

    ф

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@ const hours = parseFloat(prompt("Ведіть кількість годин"));
 const result = hours * 60;
 alert(result);
 
+const userYear = parseInt(prompt("Ведіть ваш рік народження"));
+const userCity = prompt("Ведіть із якого ви міста");
+const userSport = prompt("Яким спортом ви займаєтесь?");
+
+
+
+
