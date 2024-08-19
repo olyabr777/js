d649292efd476258035019c5f415708951ec664a
olyabr777 <158588741+olyabr777@users.noreply.github.com>
2024-08-19 19:17:44 +0300

    hm13

--- js/script.js
+++ js/script.js
@@ -1,67 +1,86 @@
 'use strict';
 
-//Вивести на сторінку в один рядок через кому числа від 10 до 20.
-// for (let i = 10; i <= 20; i++) {
-//     console.log(i);
-// }
+// Вивести на сторінку в один рядок через кому числа від 10 до 20.
+for (let i = 10; i <= 20; i++) {
+    console.log(i);
+}
 
-//Вивести квадрати чисел від 10 до 20.
-// for (let i = 10; i <= 20; i++) {
-//     console.log( Math.pow(i, 2) );
-// }
+// Вивести квадрати чисел від 10 до 20.
+for (let i = 10; i <= 20; i++) {
+    console.log( Math.pow(i, 2) );
+}
 
-//Вивести таблицю множення на 7.
-// for (let i = 1; i <= 10; i++) {
-//     let result = 7 * i;
-//     console.log( `7 x ${i} = ${result}` );
-// }
+// Вивести таблицю множення на 7.
+for (let i = 1; i <= 10; i++) {
+    let result = 7 * i;
+    console.log( `7 x ${i} = ${result}` );
+}
 
-//Знайти суму всіх цілих чисел від 1 до 15.
-// let sum = 0;
-// for (let i = 1; i <= 15; i++) {
-//     sum = sum + i;
-// }
-// console.log(sum);
+// Знайти суму всіх цілих чисел від 1 до 15.
+let sum = 0;
+for (let i = 1; i <= 15; i++) {
+    sum = sum + i;
+}
+console.log(sum);
 
-//Знайти добуток усіх цілих чисел від 15 до 35.
-// let resultMultiplication = 0;
-// for (let i = 15; i <= 35; i++) {
-//     resultMultiplication = resultMultiplication * i;
-// }
-// console.log(resultMultiplication);
+// Знайти добуток усіх цілих чисел від 15 до 35.
+let resultMultiplication = 0;
+for (let i = 15; i <= 35; i++) {
+    resultMultiplication = resultMultiplication * i;
+}
+console.log(resultMultiplication);
 
-//Знайти середнє арифметичне всіх цілих чисел від 1 до 500.
+// Знайти середнє арифметичне всіх цілих чисел від 1 до 500.
 
-// let sumArithmetic = 0;
-// for (let i = 1; i <= 500; i++) {
-//     sumArithmetic +=  i;
-//  }
-//  console.log(sumArithmetic / 500);
+let sumArithmetic = 0;
+for (let i = 1; i <= 500; i++) {
+    sumArithmetic +=  i;
+ }
+ console.log(sumArithmetic / 500);
 
-//Вивести суму лише парних чисел в діапазоні від 30 до 80.
+// Вивести суму лише парних чисел в діапазоні від 30 до 80.
 
-// let sumEven = 0;
-// for (let i = 30; i <= 80; i++) {
-//     if (i % 2 === 0) sumEven +=  i;
-// }
-// console.log(sumEven);
+let sumEven = 0;
+for (let i = 30; i <= 80; i++) {
+    if (i % 2 === 0) sumEven +=  i;
+}
+console.log(sumEven);
 
 // Вивести всі числа в діапазоні від 100 до 200 кратні 3.
 
-// for (let i = 100; i <= 200; i++) {
-//     if (i % 3 === 0){
-//         console.log(i);
-//     }
-// }
+for (let i = 100; i <= 200; i++) {
+    if (i % 3 === 0){
+        console.log(i);
+    }
+}
 
-//Дано натуральне число. Знайти та вивести на сторінку всі його дільники.
+// Дано натуральне число. Знайти та вивести на сторінку всі його дільники.
 
+const number = prompt("натуральне число");
 
+for (let i = 1; i <= number; i++) {
+    if (number % i === 0) console.log(i);
+}
 
+// Визначити кількість його парних дільників.
 
+let resultSumMod2 = 0;
 
+for (let i = 1; i <= number; i++) {
+    if (number % i === 0){
+        if (i % 2 === 0){
+            console.log(i);
+            resultSumMod2 += i;
+        }
+    } 
+    
+console.log(resultSumMod2);
 
+// Надрукувати повну таблицю множення від 1 до 10.
 
-
-
-
+for (let y = 1; y <= 10; y++) {
+    for (let i = 1; i <= 10; i++) {
+        let result = y * i;
+        console.log( `${y} x ${i} = ${result}` );
+    }
+}
