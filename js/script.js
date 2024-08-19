753bbf70bd89b784b4db975bb2fc84489b882675
olyabr777 <158588741+olyabr777@users.noreply.github.com>
2024-08-19 18:57:58 +0300

    hm_13

--- js/script.js
+++ js/script.js
@@ -1,6 +1,67 @@
 'use strict';
 
-console.log(i);
-for (let i = 10; i <= 20; i++) {
-    console.log(i);
-}
+//Вивести на сторінку в один рядок через кому числа від 10 до 20.
+// for (let i = 10; i <= 20; i++) {
+//     console.log(i);
+// }
+
+//Вивести квадрати чисел від 10 до 20.
+// for (let i = 10; i <= 20; i++) {
+//     console.log( Math.pow(i, 2) );
+// }
+
+//Вивести таблицю множення на 7.
+// for (let i = 1; i <= 10; i++) {
+//     let result = 7 * i;
+//     console.log( `7 x ${i} = ${result}` );
+// }
+
+//Знайти суму всіх цілих чисел від 1 до 15.
+// let sum = 0;
+// for (let i = 1; i <= 15; i++) {
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+//Знайти добуток усіх цілих чисел від 15 до 35.
+// let resultMultiplication = 0;
+// for (let i = 15; i <= 35; i++) {
+//     resultMultiplication = resultMultiplication * i;
+// }
+// console.log(resultMultiplication);
+
+//Знайти середнє арифметичне всіх цілих чисел від 1 до 500.
+
+// let sumArithmetic = 0;
+// for (let i = 1; i <= 500; i++) {
+//     sumArithmetic +=  i;
+//  }
+//  console.log(sumArithmetic / 500);
+
+//Вивести суму лише парних чисел в діапазоні від 30 до 80.
+
+// let sumEven = 0;
+// for (let i = 30; i <= 80; i++) {
+//     if (i % 2 === 0) sumEven +=  i;
+// }
+// console.log(sumEven);
+
+// Вивести всі числа в діапазоні від 100 до 200 кратні 3.
+
+// for (let i = 100; i <= 200; i++) {
+//     if (i % 3 === 0){
+//         console.log(i);
+//     }
+// }
+
+//Дано натуральне число. Знайти та вивести на сторінку всі його дільники.
+
+
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+console.log(i);
+for (let i = 10; i <= 20; i++) {
+    console.log(i);
+}
