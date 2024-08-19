9ad4abcdafe5ea2c360446dd0b3620a9524f44aa
olyabr777 <158588741+olyabr777@users.noreply.github.com>
2024-08-19 20:42:58 +0300

    hm_14

--- js/script.js
+++ js/script.js
@@ -1,86 +1,59 @@
 'use strict';
 
-// Вивести на сторінку в один рядок через кому числа від 10 до 20.
-for (let i = 10; i <= 20; i++) {
+// Вивести числа від 20 до 30 через пропуск, використовуючи крок 0,5 (20 20,5 21 21,5….).
+
+for (let i = 20; i <= 30; i += 0.5) {
     console.log(i);
 }
 
-// Вивести квадрати чисел від 10 до 20.
-for (let i = 10; i <= 20; i++) {
-    console.log( Math.pow(i, 2) );
+// Один долар коштує 27 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів.
+
+for (let i = 10; i <= 100; i += 10) {
+    console.log(i * 27);
 }
 
-// Вивести таблицю множення на 7.
-for (let i = 1; i <= 10; i++) {
-    let result = 7 * i;
-    console.log( `7 x ${i} = ${result}` );
+// Дане ціле число. Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N.
+
+const number = parseInt( prompt("натуральне число") );
+for (let i = 1; i <= 100; i++){
+    let pow = Math.pow(i, 2);
+    if (pow <= number) console.log(i);
 }
 
-// Знайти суму всіх цілих чисел від 1 до 15.
-let sum = 0;
-for (let i = 1; i <= 15; i++) {
-    sum = sum + i;
-}
-console.log(sum);
 
-// Знайти добуток усіх цілих чисел від 15 до 35.
-let resultMultiplication = 0;
-for (let i = 15; i <= 35; i++) {
-    resultMultiplication = resultMultiplication * i;
-}
-console.log(resultMultiplication);
+//Дане ціле число. З'ясувати, чи є воно простим (простим називається число, більше 1, які не мають інших дільників крім 1 і себе).
 
-// Знайти середнє арифметичне всіх цілих чисел від 1 до 500.
+let isPrime = true;
 
-let sumArithmetic = 0;
-for (let i = 1; i <= 500; i++) {
-    sumArithmetic +=  i;
- }
- console.log(sumArithmetic / 500);
-
-// Вивести суму лише парних чисел в діапазоні від 30 до 80.
-
-let sumEven = 0;
-for (let i = 30; i <= 80; i++) {
-    if (i % 2 === 0) sumEven +=  i;
-}
-console.log(sumEven);
-
-// Вивести всі числа в діапазоні від 100 до 200 кратні 3.
-
-for (let i = 100; i <= 200; i++) {
-    if (i % 3 === 0){
-        console.log(i);
-    }
-}
-
-// Дано натуральне число. Знайти та вивести на сторінку всі його дільники.
-
-const number = prompt("натуральне число");
-
-for (let i = 1; i <= number; i++) {
-    if (number % i === 0) console.log(i);
-}
-
-// Визначити кількість його парних дільників.
-
-let resultSumMod2 = 0;
-
-for (let i = 1; i <= number; i++) {
-    if (number % i === 0){
-        if (i % 2 === 0){
-            console.log(i);
-            resultSumMod2 += i;
+if (number <= 1) {
+    isPrime = false;
+} else if (number === 2) {
+    isPrime = true;
+} else {
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            isPrime = false;
+            break;
         }
-    } 
-    
-console.log(resultSumMod2);
-
-// Надрукувати повну таблицю множення від 1 до 10.
-
-for (let y = 1; y <= 10; y++) {
-    for (let i = 1; i <= 10; i++) {
-        let result = y * i;
-        console.log( `${y} x ${i} = ${result}` );
     }
+}
+
+if (isPrime) {
+    console.log(number + " є простим числом");
+} else {
+    console.log(number + " не є простим числом");
+}
+
+//Дане деяке число. Визначити, чи можна одержати це число шляхом зведення числа 3 у деякий ступінь. (Наприклад, числа 9, 81 можна отримати, а 13 - не можна).
+
+let powerOfThree = 1;
+
+while (powerOfThree < number) {
+    powerOfThree *= 3;
+}
+
+if (powerOfThree === number) {
+    console.log(number + " можна отримати ");
+} else {
+    console.log(number + " не можна отримати");
 }
